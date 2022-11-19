bd437cc48176d1ce758f5a7bf6b0bc10dca14316
Hooman Mohammadi <h.mohammadi.f@gmail.com>
2022-11-19 23:52:20 +0200

    End of section 5 and UI CRUD

---
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { Grid, List } from "semantic-ui-react";
+import { Activity } from "../../../app/models/Activity";
+import ActivityDetails from "../details/ActivityDetails";
+import ActivityForm from "../form/ActivityForm";
+import ActivityList from "./ActivityList";
+
+interface Props {
+  activities: Activity[];
+  selectedActivity: Activity | undefined;
+  selectActivity: (id: string) => void;
+  cancelSelectedActivity: () => void;
+  editMode: boolean;
+  openForm: (id: string) => void;
+  closeForm: () => void;
+  createOrEdit: (activity: Activity) => void;
+  deleteActivity: (id: string) => void;
+
+}
+
+export default function ActivityDashboard(props: Props) {
+  return (
+    <Grid>
+      <Grid.Column width="10">
+        <ActivityList
+          activities={props.activities}
+          selectActivity={props.selectActivity}
+          deleteActivity={props.deleteActivity}
+        />
+      </Grid.Column>
+      <Grid.Column width="6">
+        {!props.editMode && props.selectedActivity && (
+          <ActivityDetails
+            activity={props.selectedActivity}
+            cancelSelectedActivity={props.cancelSelectedActivity}
+            openForm={props.openForm}
+          />
+        )}
+        {props.editMode && (
+          <ActivityForm
+            closeForm={props.closeForm}
+            activity={props.selectedActivity}
+            createOrEdit={props.createOrEdit}
+          />
+        )}
+      </Grid.Column>
+    </Grid>
+  );
+}
---
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -0,0 +1,86 @@
+import React, { ChangeEvent, useState } from "react";
+import { Button, Form, Segment } from "semantic-ui-react";
+import { Activity } from "../../../app/models/Activity";
+
+interface Props {
+  activity: Activity | undefined;
+  closeForm: () => void;
+  createOrEdit: (activity: Activity) => void;
+}
+
+export default function ActivityForm(props: Props) {
+  const initialState = props.activity
+    ? props.activity
+    : {
+        id: "",
+        title: "",
+        date: "",
+        description: "",
+        category: "",
+        city: "",
+        venue: "",
+      };
+
+  const [enteredActivity, setEnteredActivity] = useState(initialState);
+
+  function handleSubmit() {
+    props.createOrEdit(enteredActivity);
+  }
+
+  function handleInputChange(
+    event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) {
+    const { name, value } = event.target;
+    setEnteredActivity({ ...enteredActivity, [name]: value });
+  }
+
+  return (
+    <Segment clearing>
+      <Form onSubmit={handleSubmit} autoComplete="off">
+        <Form.Input
+          placeholder="Title"
+          value={enteredActivity.title}
+          name="title"
+          onChange={handleInputChange}
+        />
+        <Form.TextArea
+          placeholder="Description"
+          value={enteredActivity.description}
+          name="description"
+          onChange={handleInputChange}
+        />
+        <Form.Input
+          placeholder="Category"
+          value={enteredActivity.category}
+          name="category"
+          onChange={handleInputChange}
+        />
+        <Form.Input
+          placeholder="Date"
+          value={enteredActivity.date}
+          name="date"
+          onChange={handleInputChange}
+        />
+        <Form.Input
+          placeholder="City"
+          value={enteredActivity.city}
+          name="city"
+          onChange={handleInputChange}
+        />
+        <Form.Input
+          placeholder="Venue"
+          value={enteredActivity.venue}
+          name="venue"
+          onChange={handleInputChange}
+        />
+        <Button floated="right" positive type="submit" content="Submit" />
+        <Button
+          onClick={props.closeForm}
+          floated="right"
+          type="button"
+          content="Cancel"
+        />
+      </Form>
+    </Segment>
+  );
+}
